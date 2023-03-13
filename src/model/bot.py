00927e25bc8c91f5d54f1a16ff731a569abdfa19
th0rz05 <73318820+th0rz05@users.noreply.github.com>
2023-03-13 18:16:19 +0000

    minimax is working with depth 0 but with depth 1 is weird

--- src/model/bot.py
+++ src/model/bot.py
@@ -1,13 +1,14 @@
 import copy
 import math
-from model.board import Board
 
 from random import choice
+
 
 class Bot:
     """
     Defines a bot player. The bot can be of different difficulty levels.
     """
+
     def __init__(self, player, difficulty):
         self.player = player
         if difficulty == 0:
@@ -22,60 +23,55 @@ class Bot:
     def play_random(self, game):
         """Play a random move."""
         return choice(game.board.get_moves(self.player))
-    
+
     def mate_in_one(self, game):
-        for move in game.board.get_moves(self.player):
-            if game.board.did_bound(move[1]):
+        for move in game.board.get_moves(game.player):
+            print(f"move: {move}")
+            newgame = copy.deepcopy(game)
+            newgame.move(move[0], move[1])
+            if newgame.board.did_bound(move[1]):
                 print(f"bound 1")
-                return 1
-        print(f"bound 0")
-        return 0
-            
+                return 1, move
+        print("no bound possible")
+        return 0, move
+
     def play_with_minimax(self, game):
         """Play a move using the minimax algorithm."""
-        return self.minimax(game, 1, -math.inf,math.inf,True,self.mate_in_one)
-    
-    
-    def minimax(self,game,depth,alpha,beta,maximizing,evaluate_func):
-        print(f"depth {depth}")
+        print("playing")
+        _ , move = self.minimax(game, 0, -math.inf, math.inf, True, self.mate_in_one)
+        return move
+
+
+    def minimax(self, game, depth, alpha, beta, maximizing, evaluate_func):
         if depth == 0 or game.over:
             return evaluate_func(game)
         if maximizing:
             maxEval = -math.inf
+            bestMove = None
             for move in game.board.get_moves(game.player):
-                print(f"move {move}")
-                print(f"player {game.player}")
                 newgame = copy.deepcopy(game)
-                newgame.move(move[0],move[1])
-                evaluation = self.minimax(newgame,depth-1,alpha,beta,False,evaluate_func)
-                maxEval = max(maxEval, evaluation)
-                print(f"max {maxEval}")
-                print(f"eval {evaluation}")
-                print(f"alpha1 {alpha}")
+                newgame.move(move[0], move[1])
+                evaluation, _ = self.minimax(
+                    newgame, depth - 1, alpha, beta, False, evaluate_func)
+                if evaluation > maxEval:
+                    maxEval = evaluation
+                    bestMove = move
                 alpha = max(alpha, evaluation)
-                print(f"beta1 {beta}")
-                if beta <= alpha: 
-                    print(f"break1")
+                if beta <= alpha:
                     break
-            return maxEval
+            return maxEval, bestMove
         else:
             minEval = math.inf
-            
+            bestMove = None
             for move in game.board.get_moves(game.player):
-                print(f"move2 {move}")
-                print(f"player2 {game.player}")
-                game.move(move[0],move[1])
-                evaluation = self.minimax(game,depth-1,alpha,beta,True,evaluate_func)
-                print(f"min {minEval}")
-                print(f"eval2 {evaluation}")
-                minEval = min(minEval, evaluation)
-                print(f"beta2 {beta}")
+                newgame = copy.deepcopy(game)
+                newgame.move(move[0], move[1])
+                evaluation, _ = self.minimax(
+                    newgame, depth - 1, alpha, beta, True, evaluate_func)
+                if evaluation < minEval:
+                    minEval = evaluation
+                    bestMove = move
                 beta = min(beta, evaluation)
-                print(f"alpha2 {alpha}")
-                if beta <= alpha: 
-                    print(f"break2")
+                if beta <= alpha:
                     break
-            return minEval
-
-
-
+            return minEval, bestMove
