a7a7eaa23cbb4c57367e0611a0979883e9e22d9c
th0rz05 <73318820+th0rz05@users.noreply.github.com>
2023-04-03 17:53:17 +0100

    better testing facilities

--- src/main_dev.py
+++ src/main_dev.py
@@ -14,17 +14,21 @@ def main_dev():
     average_time_player_1 = 0
     average_time_player_2 = 0
     count = 0
-    games = 2
+    games = 30
     
     for i in range(games):
         print("Game: ", i)
         # create a game controller with two bots
         player1 = Bot(1,"Tiago")
+        player1.bot_settings.minimax_depth = 3
+        player1.bot_settings.minimax_evaluate = 3
         player1.bot_settings.montecarlo_exploration = 2
         player1.bot_settings.montecarlo_simulations = 2000
-        player2 = Bot(2,"Martim")
+        player2 = Bot(2,"Tiago")
         player2.bot_settings.minimax_depth = 3
         player2.bot_settings.minimax_evaluate = 3
+        player2.bot_settings.montecarlo_exploration = 2
+        player2.bot_settings.montecarlo_simulations = 2000
         #board = 1 -> 4x5 board | board = 2 -> 6x5 board | board = 3 -> 8x5 board
         game_controller = GameController(player1, player2,None,1) 
         # play the game
@@ -44,10 +48,10 @@ def main_dev():
     #print the results
     print("\n\n\nFINAL RESULTS")
     print("Number of games: ", games)
-    print("Player 1 wins: ", player1_wins)
-    print("Player 2 wins: ", player2_wins)
-    print("Average time player 1: ", average_time_player_1)
-    print("Average time player 2: ", average_time_player_2)
+    print("Player 1 BLACK wins: ", player1_wins)
+    print("Player 2 WHITE wins: ", player2_wins)
+    print("Average time player 1 BLACK: ", average_time_player_1)
+    print("Average time player 2 WHITE: ", average_time_player_2)
     
     
     
